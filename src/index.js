f139c9ed128145201650063fee6b60c8f74fee57
Linas <ketleriuslinas@gmail.com>
2025-06-27 09:31:52 +0300

    lesson 3

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { PageRegister } from './pages/PageRegister.js';
 import { Page404 } from './pages/Page404.js';
 import { PageServices } from './pages/PageServices.js';
 import { PageServiceInner } from './pages/PageServiceInner.js';
+import { PageServiceDelete } from './pages/PageServiceDelete.js';
 
 const app = express();
 const port = 3000;
@@ -14,6 +15,8 @@ app.use(express.static('public'));
 app.get('/', (req, res) => res.send(new PageHome(req).render()));
 app.get('/about', (req, res) => res.send(new PageAbout(req).render()));
 app.get('/services', (req, res) => res.send(new PageServices(req).render()));
+app.get('/services', (req, res) => res.send(new PageServiceDelete(req).render()));
+
 app.get('/services/:service', (req, res) => res.send(new PageServiceInner(req).render()));
 
 app.get('/register', (req, res) => res.send(new PageRegister(req).render()));
