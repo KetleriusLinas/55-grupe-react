233ccfd0ad5f5114d213209ec69dd6dd7fa5d05e
Linas <ketleriuslinas@gmail.com>
2025-07-28 09:41:35 +0300

    text list

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import logo from '../assets/react.svg';
 
 export function Header() {
     return (
-        <div class="container">
+        <div className="container">
             <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom"> 
                 <a href="/"
                 className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"> 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,34 @@
+import { TextList } from "../components/text-list/TextList";
 
 export function Home() {
+    const list = ['Pomidoras', 'Agurkas', 'Svogunas', 'Cesnakas', 'Bulve'];
+    const liList = list.map(text => <li key={text}>{text}</li>);
+    // list.map(text => <li>{text}</li>
+    // for (const text of list) {
+
+    //     if (text.length > 5){
+    //         liList.push(<li>{text}</li>)
+    //     }
+    // }
     return (
         <>
             <main>
-                HOME PAGE
+                <section className="container">
+                    <div className="row">
+                        <div className="col-12 mb-5">
+                            <ul><TextList /></ul>
+                        </div>
+                    </div>
+                </section>
+                <section className="container">
+                    <div className="row">
+                        <div className="col-12 mb-5">
+                            <ul>
+                                {liList}
+                            </ul>
+                        </div>
+                    </div>
+                </section>
             </main>
         </>
     );
