3ab1532f355ac873fda31c68a96f12625634b7d1
Linas <ketleriuslinas@gmail.com>
2025-07-29 10:28:24 +0300

    header/footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,33 +1,56 @@
 import { Link } from "react-router";
+import { menuData } from "../data/MenuData";
+
 export function Footer() {
     return (
         <div className="container">
             <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-                <p className="col-md-4 mb-0 text-body-secondary">© 2025 Company, Inc</p> <a href="/"
-                    className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
-                    aria-label="Bootstrap"> <svg className="bi me-2" width="40" height="32" aria-hidden="true">
-                    </svg> </a>
-                <ul className="nav col-md-4 justify-content-end">
-                    <li className="nav-item">
-                        <Link to="/" className="nav-link">Home</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/todo" className="nav-link">Todo</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/about" className="nav-link">About</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/services" className="nav-link">Services</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/login" className="nav-link">Login</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/register" className="nav-link">Register</Link>
-                    </li>
+                <p className="col-md-4 mb-0 text-body-secondary">&copy; 2025 Company, Inc</p>
+                <ul className="nav col-md-8 justify-content-end">
+                    {
+                        menuData.map(item => (
+                            <li key={item.href} className="nav-item">
+                                <Link className='nav-link' to={item.href}>{item.text}</Link>
+                            </li>
+                        ))
+                    }
                 </ul>
             </footer>
         </div>
     );
 }
+
+
+// import { Link } from "react-router";
+// export function Footer() {
+//     return (
+//         <div className="container">
+//             <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+//                 <p className="col-md-4 mb-0 text-body-secondary">© 2025 Company, Inc</p> <a href="/"
+//                     className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
+//                     aria-label="Bootstrap"> <svg className="bi me-2" width="40" height="32" aria-hidden="true">
+//                     </svg> </a>
+//                 <ul className="nav col-md-4 justify-content-end">
+//                     <li className="nav-item">
+//                         <Link to="/" className="nav-link">Home</Link>
+//                     </li>
+//                     <li className="nav-item">
+//                         <Link to="/todo" className="nav-link">Todo</Link>
+//                     </li>
+//                     <li className="nav-item">
+//                         <Link to="/about" className="nav-link">About</Link>
+//                     </li>
+//                     <li className="nav-item">
+//                         <Link to="/services" className="nav-link">Services</Link>
+//                     </li>
+//                     <li className="nav-item">
+//                         <Link to="/login" className="nav-link">Login</Link>
+//                     </li>
+//                     <li className="nav-item">
+//                         <Link to="/register" className="nav-link">Register</Link>
+//                     </li>
+//                 </ul>
+//             </footer>
+//         </div>
+//     );
+// }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,35 +1,62 @@
-import { NavLink } from "react-router";
+import { NavLink } from 'react-router';
 import logo from '../assets/react.svg';
+import { menuData } from '../data/MenuData';
 
 export function Header() {
     return (
         <div className="container">
             <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-                <a href="/"
-                    className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+                <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
                     <img src={logo} alt='Logo' />
-                    <span className="fs-4">Simple header</span> </a>
+                    <span className="fs-4">Simple header</span>
+                </a>
                 <ul className="nav nav-pills">
-                    <li className="nav-item">
-                        <NavLink to="/" className="nav-link">Home</NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink to="/todo" className="nav-link">Todo</NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink to="/about" className="nav-link">Cookies</NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink to="/services" className="nav-link">Services</NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink to="/login" className="nav-link">Login</NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink to="/register" className="nav-link">Register</NavLink>
-                    </li>
+                    {
+                        menuData.map(item => (
+                            <li key={item.href} className="nav-item">
+                                <NavLink className='nav-link' to={item.href}>{item.text}</NavLink>
+                            </li>
+                        ))
+                    }
                 </ul>
             </header>
         </div>
     );
-};
+}
+
+
+// import { NavLink } from "react-router";
+// import logo from '../assets/react.svg';
+
+// export function Header() {
+//     return (
+//         <div className="container">
+//             <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+//                 <a href="/"
+//                     className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+//                     <img src={logo} alt='Logo' />
+//                     <span className="fs-4">Simple header</span> </a>
+//                 <ul className="nav nav-pills">
+//                     <li className="nav-item">
+//                         <NavLink to="/" className="nav-link">Home</NavLink>
+//                     </li>
+//                     <li className="nav-item">
+//                         <NavLink to="/todo" className="nav-link">Todo</NavLink>
+//                     </li>
+//                     <li className="nav-item">
+//                         <NavLink to="/about" className="nav-link">Cookies</NavLink>
+//                     </li>
+//                     <li className="nav-item">
+//                         <NavLink to="/services" className="nav-link">Services</NavLink>
+//                     </li>
+//                     <li className="nav-item">
+//                         <NavLink to="/login" className="nav-link">Login</NavLink>
+//                     </li>
+//                     <li className="nav-item">
+//                         <NavLink to="/register" className="nav-link">Register</NavLink>
+//                     </li>
+//                 </ul>
+//             </header>
+//         </div>
+//     );
+// };
